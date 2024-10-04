01d1d0c484525c6d937b61ddf869ea5142df8ca7
mikii <108529128+mikii1i333@users.noreply.github.com>
2024-10-04 20:48:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'HISTORIA',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'HISTORIA',
+    'Qual evento marcou o início da Segunda Guerra Mundial?',
+    'A invasão da Polônia pela Alemanha'
+)
+
+criaCartao(
+    'LINGUA INGLESA',
+    'Qual significado da gíria tbh?',
+    'Para ser honesto "to be honest"'
+)
+
+criaCartao(
+    'LINGUA INGLESA',
+    'Qual o signiicado da gíria fr?',
+    'Significa afirmar algo verdadeiro "for real"'
+)
+criaCartao(
+    'LINGUA PORTUGUESA',
+    'Qual é o autor da obra "Dom Casmurro" ?',
+    'Machado De Assis'
+)
+
+criaCartao(
+    'LINGUA PORTUGUESA',
+    'Qual é o nome da obra mais famosa de Jose Alencar?',
+    'O Guarani'
+)
